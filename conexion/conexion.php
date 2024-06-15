c9fb824645529fd4b826a0d2c39b3504d641c099
mizael763 <145491624+mizael763@users.noreply.github.com>
2024-06-15 12:08:40 -0500

    Update conexion.php
    cc

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,24 +1,9 @@
 <?php
-/*
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "el_punto_de_hierro";
-
-try {
-    // Crear la conexi��n PDO
-    $conexion = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Configurar el modo de error para que PDO lance excepciones en caso de errores
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    // Si hay alg��n error en la conexi��n, mostrar el mensaje de error
-    echo "Conexiion fallida: " . $e->getMessage();
-}*/
 $dsn = 'mysql:host=localhost;dbname=el_punto_de_hierro;charset=utf8mb4';
 $username = 'root';
 $password = '';
 
-$conn = 'lros jwaj lkel dpzo';
+$conn = '';//quite la contraseña de acseso a mi correo
 try {
     $conexion = new PDO($dsn, $username, $password);
     // Establecer el modo de error de PDO a excepción
